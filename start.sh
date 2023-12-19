c461a0d3d2d680b6d5ff99d3c1da5ec3b2a3d7dc
KingCobra91 <117145604+KingCobra91@users.noreply.github.com>
2023-12-19 12:56:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/cobra1294/MARTIN-BOT-2.git /MARTIN-BOT-2 
+  git clone https://github.com/KingCobra91/Lastest-Don-Filter.git /Lastest-Don-Filter
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /MARTIN-BOT-2 
+  git clone $UPSTREAM_REPO /Lastest-Don-Filter 
 fi
-cd /MARTIN-BOT-2 
+cd /Lastest-Don-Filter 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
